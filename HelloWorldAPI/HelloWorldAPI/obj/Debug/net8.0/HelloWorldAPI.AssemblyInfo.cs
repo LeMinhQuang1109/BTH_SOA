eb76cf372366cba02b92d7475d4c2904e7e8b4c0
LeMinhQuang1109 <lquang1109@gmail.com>
2024-12-25 15:37:50 +0700

    Quang upload

--- HelloWorldAPI/HelloWorldAPI/obj/Debug/net8.0/HelloWorldAPI.AssemblyInfo.cs
+++ HelloWorldAPI/HelloWorldAPI/obj/Debug/net8.0/HelloWorldAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorldAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68ff443a500b2db6ef2c0f6d0ab378583b26aad4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5cec560bbf1f5058a2b836b03f102e4ed3307c66")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorldAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorldAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
